5e14a1276fe1d19aecfb892171e7de29c413deb4
F3lip8 <128184907+F3lip8@users.noreply.github.com>
2023-11-29 17:45:15 -0300

    Adicionando Pagina de produto

---
+++ scripts/script-page-1.js
@@ -0,0 +1,25 @@
+var img1 = document.getElementById('img-1');
+var img2 = document.getElementById('img-2');
+var img3 = document.getElementById('img-3');
+var img4 = document.getElementById('img-4');
+var img5 = document.getElementById('img-5');
+
+img1.onclick = function(){
+    document.getElementById('main-img').src = 'Imagens/small-images/mario-verso-small.jpg';
+}
+
+img2.onclick = function(){
+    document.getElementById('main-img').src = 'Imagens/small-images/mario-gameplay-small.jpg';
+}
+
+img3.onclick = function(){
+    document.getElementById('main-img').src = 'Imagens/small-images/mario-verso-small.jpg';
+}
+
+img4.onclick = function(){
+    document.getElementById('main-img').src = 'Imagens/small-images/mario-verso-small.jpg';
+}
+
+img5.onclick = function(){
+    document.getElementById('main-img').src = 'Imagens/small-images/mario-verso-small.jpg';
+}
